9b13389b770c2791a161da37cf6976dba626c3d3
Ubuntu 14.04 <tfcom@udel.edu>
2017-10-05 11:41:37 -0400

    Need to figure out how to make more than one shark at different times

--- MovingShark/Move.java
+++ MovingShark/Move.java
@@ -18,30 +18,30 @@ public class Move extends JPanel{
 	
 	final static int frameWidth =800;
     	final static int frameHeight = 500;
-    	final static int imgWidth = 5;
-    	final static int imgHeight = 5;
+   	
     	
-    	
-    	public Move(){
-    		Animal character = new Shark(900,200,-10,"shark");
-		xloc = character.getXloc();
+    	public Move(Animal character){
+    		xloc = character.getXloc();
 		yloc = character.getYloc();
 		deltax = character.getDeltax();
 		picName = character.getpicName();
     		img = createImage();
     	}
     	public void paint(Graphics g) {
+    		if (xloc < -100)
+    			xloc = 900;
     		g.drawImage(img, xloc+=deltax, yloc, Color.gray, this);
     	}
     	
 	public static void main(String args[]){
 		JFrame frame = new JFrame();
-		
-	    	frame.getContentPane().add(new Move());
+		Animal character = new Shark(900,200,-10,"shark");
+	    	frame.getContentPane().add(new Move(character));
 	    	frame.setBackground(Color.gray);
 	    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    	frame.setSize(frameWidth, frameHeight);
 	    	frame.setVisible(true);
+	    	
 	    	for(int i = 0; i < 1000; i++){
 	    		frame.repaint();
 	    		try {
