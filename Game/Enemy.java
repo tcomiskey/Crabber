2b751d43c946b8979e1415af7d95850d37e969cf
Ubuntu 14.04 <tfcom@udel.edu>
2017-10-24 18:09:29 -0400

    Question updates and Question list

--- Game/Enemy.java
+++ Game/Enemy.java
@@ -1,10 +1,25 @@
+import java.io.*;
+import java.util.*
+
 public class Enemy extends Character{
+	private Question[] questions;
 	
 	public Enemy(int x, int y){
 		super(x, y);
+		File qAndAFile = new File("QuestionAndAnswerList.txt");
+		if myFile.exists(){
+			Scanner fileScanner = new Scanner(qAndAFile);
+			while(fileScanner.hasNext()){
+				ArrayList <String> questionSpecifics = new ArrayList<String>[4];
+				for (int newLineCount = 0, newLineCount<4, newLineCount++){
+					questionSpecifics[newLineCount] = fileScanner.next();
+				}
+				questions[i] = new Question(questionSpecifics);
+			}
+		}
 	}
-	public void hit(){
-		//get shit on
+	public Question hit(){
+		return 
 	}
 	
 }
