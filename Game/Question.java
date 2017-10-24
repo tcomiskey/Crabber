6779de02b17c5f941ab71ba4517e62a79a436654
erinhitchner <ehitch@udel.edu>
2017-10-24 18:23:14 -0400

    defined constructor and toString

--- Game/Question.java
+++ Game/Question.java
@@ -1,3 +1,18 @@
 public class Question {
-
+    String question;
+    String rightAnswer;
+    String wrongAnswer1;
+    String wronganswer2;
+    
+    public Question(ArrayList<String> questionSpecifics) {
+        question = questionSpecifics[1]
+        rightAnswer = questionSpecifics[2]
+        wrongAnswer1 = questionSpecifics[3]
+        wronganswer2 = questionSpecifics[4]
+    }
+    
+    public String toString() {
+        return "Question: " + question + "\n" + rightAnswer + "\n" + wrongAnswer1 + "\n" + wronganswer2
+    }
+    
 }
