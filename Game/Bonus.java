26d14e3eb94d2b43db62eeb517450038d00a4571
erinhitchner <ehitch@udel.edu>
2017-10-24 20:19:34 -0400

    sorta did the bonus class

--- Game/Bonus.java
+++ Game/Bonus.java
@@ -1,3 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.lang.Math;
 public class Bonus extends GameCharacter{
+    private static ArrayList<String> bonusMessage = new ArrayList<String>(4);
 
+    public Bonus(int x, int y) {
+        //super(x,y);
+    }
+    
+    public Question hit() {
+        bonusMessage.add("You ate a worm! Enemies will be slower!");
+        bonusMessage.add("");
+        bonusMessage.add("");
+        bonusMessage.add("");
+        return new Question(bonusMessage);
+    }
+    
 }
