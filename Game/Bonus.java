2bcf4293602d1574ed043a42997bae961278e364
Ubuntu 14.04 <tfcom@udel.edu>
2017-10-26 13:46:06 -0400

    Added bonus message class

--- Game/Bonus.java
+++ Game/Bonus.java
@@ -2,18 +2,13 @@ import java.io.*;
 import java.util.*;
 import java.lang.Math;
 public class Bonus extends GameCharacter{
-    	private static ArrayList<String> bonusMessage = new ArrayList<String>(4);
 
     	public Bonus(int x, int y) {
         //super(x,y);
     	}
     
-	public Question hit() {
-		bonusMessage.add("You ate a worm! Enemies will be slower!");
-		bonusMessage.add("");
-		bonusMessage.add("");
-		bonusMessage.add("");
-		return new Question(bonusMessage);
+	public Message hit() {
+		return new BonusMessage();
 	}
     	public int getX(){
 		return xLoc;
---
+++ Game/BonusMessage.java
@@ -0,0 +1,9 @@
+public class BonusMessage extends Message{
+	private String bonusMessage;
+	public BonusMessage(){
+		bonusMessage = "You ate a worm! Enemies will be slower!";
+	}
+	public String toString(){
+		return bonusMessage;
+	}
+}
