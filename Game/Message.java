d339ca163b868ff191ffe10a819ceab26de2f5d6
erinhitchner <ehitch@udel.edu>
2017-10-29 16:59:40 -0400

    made a pop up question

--- Game/Message.java
+++ Game/Message.java
@@ -7,5 +7,18 @@ public Message() {
 public String toString() {
     return "Message Object";
 }
-
+    public String getQuestion() {
+        return "message method used";
+    }
+    public String getRightAnswer() {
+        return "message method used";
+    }
+    public String getWrongAnswer1() {
+        return "message method used";
+    }
+    public String getWrongAnswer2() {
+        return "message method used";
+    }
+    
+    
 }
